b924ad50793dbe043699bcd37b33175389e87f13
Karter Krueger <krueger.karter@gmail.com>
2022-09-06 16:54:34 -0400

    Full stitching working. Needs blending.

--- Phase1/Code/Wrapper.py
+++ Phase1/Code/Wrapper.py
@@ -25,15 +25,14 @@ import os
 # def main():
 # Add any Command Line arguments here
 from matplotlib import pyplot as plt
-from usb._lookup import descriptors
 
 Parser = argparse.ArgumentParser()
 Parser.add_argument('--NumFeatures', default=1000,
                     help='Number of best features to extract from each image, Default:1000')
-Parser.add_argument('--DataPath', default="../Data/Train/Set1", help='Path to the dataset folder to stitch')
+Parser.add_argument('--DataPath', default="../Data/Train/Set2", help='Path to the dataset folder to stitch')
 # TODO: tune these thresholds
-Parser.add_argument('--MatchRatioThreshold', default=.85)
-Parser.add_argument('--TauThreshold', default=200)
+Parser.add_argument('--MatchRatioThreshold', default=.75)
+Parser.add_argument('--TauThreshold', default=160)
 Parser.add_argument('--RansacNMax', default=500)
 
 Args = Parser.parse_args()
@@ -50,7 +49,7 @@ PatchSize = 41
 """
 Read a set of images for Panorama stitching
 """
-img_paths = glob.glob(os.path.join(DataPath, "*.jpg"))
+img_paths = sorted(glob.glob(os.path.join(DataPath, "*.jpg")))
 imgs = [cv2.imread(path) for path in img_paths]
 # img = imgs[2]
 imgs_gray = [cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) for img in imgs]
@@ -138,7 +137,7 @@ def get_descriptors(img_gray, corners_best):
                            patch,
                            np.tile(patch[-1:, :], (max(0, bottom - y_max), 1))])
 
-        patch_gaussian = cv2.GaussianBlur(patch, (3, 3), 3)
+        patch_gaussian = cv2.GaussianBlur(patch, (5, 5), 3)
         sub_sample_size = (8, 8)
         x_range = np.linspace(sub_sample_size[0] // 2, PatchSize - sub_sample_size[0] // 2, sub_sample_size[0],
                               dtype=int)
@@ -149,7 +148,7 @@ def get_descriptors(img_gray, corners_best):
 
         sub_sample2 = cv2.resize(patch_gaussian, sub_sample_size)
         # TODO: allowed to use this? for better consistency?
-        sub_sample = sub_sample2
+        # sub_sample = sub_sample2
 
         desc_vector = sub_sample.reshape((-1,)).astype(float)
         variance = np.var(desc_vector)
@@ -178,6 +177,7 @@ def match(feature_descriptors_1, feature_descriptors_2, corners1=None, corners2=
     for i, desc_i in enumerate(feature_descriptors_1):
         diffs = feature_descriptors_2 - desc_i
         ssd = np.sum(np.square(diffs), axis=1)
+        # print(np.sort(ssd))
         sorted_ssd_args = np.argsort(ssd)
         j = sorted_ssd_args[0]
         ratio = ssd[j] / ssd[sorted_ssd_args[1]]
@@ -235,7 +235,7 @@ def ransac(corners1, corners2, matches_ij, img1=None, img2=None):
             H1 = np.linalg.inv(matrix) @ v
             H1 = H1.reshape((3, 3))
         except:
-            print("failed to compute H")
+            # print("failed to compute H")
             continue
 
         # TODO: fix scratch homography
@@ -255,7 +255,6 @@ def ransac(corners1, corners2, matches_ij, img1=None, img2=None):
             print("Over 90% inliers!")
             break
 
-        print("a")
     if img1 is not None:
         matches_img = np.hstack([img1, img2])
         out = cv2.drawMatches(img1, [cv2.KeyPoint(x, y, 3) for x, y in corners1_matched.astype(float)],
@@ -264,7 +263,7 @@ def ransac(corners1, corners2, matches_ij, img1=None, img2=None):
         cv2.imwrite("ransac.png", out)
     max_inliers_H = cv2.findHomography(corners1_matched[max_inliers_list], corners2_matched[max_inliers_list], 0)[0]
 
-    return max_inliers_H
+    return max_inliers_H, max_inliers_list
 
 
 """
@@ -276,21 +275,21 @@ Save Panorama output as mypano.png
 # Then combine these matrices based on the Homography position
 # Blend the edges between image pairs
 # cv2.imwrite('mypano.png', pano_img)
-def stitch(img1, img2, ransac_H_out):
-    H1 = np.eye(3)
+def stitch(img1, img2, H1, H2):
+    # H1 = np.eye(3)
     # H1[0, -1] += 0
     # H1[1, -1] += 0
-    t = [400, 400]
-    Ht = np.array([[1, 0, t[0]], [0, 1, t[1]], [0, 0, 1]])  # translate
+    t = [600, 600]
+    Ht = np.array([[1, 0, t[0]], [0, 1, t[1]], [0, 0, 1]]).astype(float)  # translate
 
-    H2 = np.linalg.inv(ransac_H_out)
+    # H2 = np.linalg.inv(H2)
     # H2[0, -1] += 0
     # H2[1, -1] += 0
     H1 = Ht.dot(H1)
     H2 = Ht.dot(H2)
 
-    img1_warp = cv2.warpPerspective(img1, H1, (img1.shape[1] * 3, img1.shape[0] * 3))
-    img2_warp = cv2.warpPerspective(img2, H2, (img2.shape[1] * 3, img2.shape[0] * 3))
+    img1_warp = cv2.warpPerspective(img1, H1, (img1.shape[1] * 4, img1.shape[0] * 4))
+    img2_warp = cv2.warpPerspective(img2, H2, (img2.shape[1] * 4, img2.shape[0] * 4))
     # img_combo = cv2.bitwise_or(img1, img2)
     overlap_mask = np.any(img1_warp > 0, axis=2) & np.any(img2_warp > 0, axis=2)
     img_combo = (img1_warp * ~np.atleast_3d(overlap_mask)).astype(float) + img2_warp.astype(float)
@@ -311,17 +310,123 @@ def check_and_stitch(i, j):
     if len(img0_1_matches) < 20:
         return None
 
-    ransac_H_out = ransac(best_corners[i], best_corners[j], img0_1_matches, img1, img2)
+    ransac_H_out, inliers = ransac(best_corners[i], best_corners[j], img0_1_matches, img1, img2)
+    # print("det", np.linalg.det(ransac_H_out))
+    if np.linalg.det(ransac_H_out) < .3:
+        return None
     out = stitch(img1, img2, ransac_H_out)
     return out
 
-img_combo01 = check_and_stitch(0, 1)
-img_combo12 = check_and_stitch(0, 2)
-overlap_mask = np.any(img_combo01 > 0, axis=2) & np.any(img_combo12 > 0, axis=2)
-img_combo = (img_combo12 * ~np.atleast_3d(overlap_mask)).astype(float) + img_combo01.astype(float)
-cv2.imwrite("mypano_full.png", img_combo)
+MIN_MATCHES_RATIO = .25
 
-print('a')
+connectivity_matrix = np.zeros((len(imgs_gray), len(imgs_gray)))
+connectivity_transforms = np.full((len(imgs_gray), len(imgs_gray), 3, 3), np.eye(3))
+
+pairs = {}
+for i, img_gray_i in enumerate(imgs_gray):
+    for j, img_gray_j in enumerate(imgs_gray):
+        if i == j:# or (i, j) in pairs.keys() or (j, i) in pairs.keys():
+            continue
+        matches = match(corners_descriptors[i], corners_descriptors[j])
+        if (len(matches) / ((len(best_corners[i]) + len(best_corners[j]))/2)) < .2:
+            continue
+        ransac_H, inliers = ransac(best_corners[i], best_corners[j], matches, img_gray_i, img_gray_j)
+        # print('det', (i, j), np.linalg.det(ransac_H))
+        if np.linalg.det(ransac_H) < .3:
+            # print("Bad transform", np.linalg.det(ransac_H))
+            continue
+        match_score = (len(inliers) / len(matches))
+        if match_score > MIN_MATCHES_RATIO:
+            # print("match", (i, j), match_score)
+            pairs[(i, j)] = match_score
+            connectivity_matrix[i, j] = len(inliers)
+            # connectivity_matrix[j, i] = match_score
+            connectivity_transforms[i, j, :, :] = ransac_H
+            # connectivity_transforms[j, i, :, :] = ransac_H
+        # else:
+            # print("no match", (i, j), match_score)
+
+best_core_img = 0
+best_score = 0
+for i in range(connectivity_matrix.shape[0]):
+    if np.count_nonzero(connectivity_matrix[i, :]) < 2:
+        continue
+    score = np.sum(connectivity_matrix[i, :])
+    if score > best_score:
+        best_core_img = i
+        best_score = score
+
+# known_imgs = [best_core_img]
+# pairs = [(best_core_img, best_core_img)]
+# unknown_imgs = set(range(connectivity_matrix.shape[1]))
+# unknown_imgs.remove(best_core_img)
+#
+# while len(unknown_imgs) > 0:
+#     for j in unknown_imgs.copy():
+#         if np.count_nonzero(connectivity_matrix[:, j]) < 1 and j in unknown_imgs:
+#             unknown_imgs.remove(j)
+#             continue
+#         if np.count_nonzero(connectivity_matrix[known_imgs, j]) < 1 or j in [p[0] for p in pairs]:
+#             continue
+#         best_starter = np.argsort(connectivity_matrix[known_imgs, j])[-1]
+#         best_starter = known_imgs[best_starter]
+#         print(best_starter, "->", j)
+#         known_imgs.append(j)
+#         unknown_imgs.remove(j)
+#         pairs.append((best_starter, j))
+#
+# running_H = np.eye(3)
+# for j in range(connectivity_matrix.shape[1]):
+#     if j == best_core_img:
+#         continue
+
+combos = []
+last_H = np.eye(3)
+
+# last_H = Ht.dot(np.eye(3))
+center = len(imgs_gray) // 2
+for i in range(center, len(imgs_gray)-1):
+    j = i+1
+
+    # H2[0, -1] += 0
+    # H2[1, -1] += 0
+    # H1 = Ht.dot(H1)
+    # H2 = Ht.dot(H2)
+
+    next_H = np.linalg.inv(connectivity_transforms[i, j]).dot(last_H)
+
+    combos.append(stitch(imgs[i], imgs[j], last_H, next_H))
+    last_H = next_H
+
+last_H = np.eye(3)
+for i in range(center, 0, -1):
+    j = i-1
+    next_H = np.linalg.inv(connectivity_transforms[i, j]).dot(last_H)
+
+    combos.insert(0, stitch(imgs[i], imgs[j], last_H, next_H))
+    last_H = next_H
+
+
+
+# img_combo01 = check_and_stitch(0, 3)
+# cv2.imwrite("mypano_01.png", img_combo01)
+
+def merge(img1, img2):
+    overlap_mask = np.any(img1 > 0, axis=2) & np.any(img2 > 0, axis=2)
+    img_combo = (img1 * ~np.atleast_3d(overlap_mask)).astype(float) + img2.astype(float)
+    return img_combo
+
+out = combos[center]
+for i in range(center+1, len(combos)):
+    out = merge(out, combos[i])
+
+for i in range(center-1, -1, -1):
+    out = merge(combos[i], out)
+
+# img_combo12 = check_and_stitch(0, 2)
+cv2.imwrite("mypano_full.png", out[200:2100, :])
+
+# print('a')
 
 # if __name__ == "__main__":
 #     main()
